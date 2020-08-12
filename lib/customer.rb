8d988b96db76f01022edf7be38e6ca8231836522
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 13:57:06 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
 class Customer
-  
+  attr_reader :name, :age
+
+@@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+def self.all
+  @@all
+end
+
+def new_meal(waiter, total, tip)
+  meal = Meal.new(waiter,self,total,tip)
+end
+
+def meals
+  Meal.all.find_all {|meal| meal.customer == self}
+end
+
+def waiters
+  self.meals.map {|meal| meal.waiter}
+end
+
+
+
+
+
+
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,40 @@
 class Waiter
+attr_accessor :years_of_experience
+attr_reader :name
+
+@@all = []
+
+def initialize(name, years)
+  @name = name
+  @years_of_experience = years
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_meal(customer, total, tip)
+  meal = Meal.new(self, customer, total, tip)
+end
+
+def meals
+  Meal.all.find_all {|meal| meal.waiter == self}
+end
+
+def best_tipper
+  highest = Meal.all.first
+  Meal.all.each do |meal|
+    if meal.tip > highest.tip
+    highest = meal
+  end
+end
+highest.customer
+end
+
+
+
+
+
 
 end
