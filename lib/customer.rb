5b799e47c286057b363670edf1565cc4dd742aec
Aaron Brady <aleebrady@gmail.com>
2020-08-13 06:03:47 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,21 @@
 class Customer
   
+  attr_accessor :name, :assert_generates
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
 end
