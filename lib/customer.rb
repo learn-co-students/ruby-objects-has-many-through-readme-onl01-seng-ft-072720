36118cafaa8f3511c804c00c03241cef52e27ff0
Bria White <bria.awhite@gmail.com>
2020-08-14 17:13:26 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
   
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)   #(the initialized instance of meal)
+  end  #creates new meals and associates that meal to the customer that created it  
+  
+  def meals
+   Meal.all.select do |meal|
+     meal.customer == self # checking for all the meals the customer had
+   end
+  end
+  
+  def waiters
+   meals.map do |meal|
+     meal.waiter # uses meals method to return all the waiters from every meal 
+   end
+  end 
+  
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
 class Waiter
+  
+attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+     meal.waiter == self #checking for waiter, all the meal he/she served
+    end
+  end
+  
+  def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end  #uses the array from meals to return the best tipping customer
+ 
+  best_tipped_meal.customer
+end
 
 end
