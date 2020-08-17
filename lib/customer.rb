d6d33d07d0b646ca2b371fd22421fb45663bc5b0
Cadetyouri <cadet.jesusyouri@gmail.com>
2020-08-17 14:16:16 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
+    attr_accessor :name, :age
+    @@all = []
   
-end
+    def initialize(name,age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    # A Customer creates a Meal, passing in a Waiter instance
+    def new_meal(waiter, total, tip=0)
+      Meal.new(waiter, self, total, tip)
+    end
+  
+    # In plain English, the customer is going to look at all of the meals, and then select only the ones that belong to them. Translated into code, that could be written like the following:
+    # We're iterating through every instance of Meal and returning only the ones where the meal's customer matches the current customer instance.
+    def meals
+      Meal.all.select do |meal|
+          meal.customer == self
+      end
+    end
+      def waiters
+          meals.map do |meal|
+              meal.waiter
+          end
+      end
+   
+  end
